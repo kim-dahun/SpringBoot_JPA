54f891ccd61c952a6eb27cd1ae0f86f5ff24ead3
kim-dahun <117079872+kim-dahun@users.noreply.github.com>
2023-09-26 17:04:30 +0900

    20230926-1

---
+++ src/test/java/jpabook/jpashop/domain/Member/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.domain.Member;
+
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    void save() {
+
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    void find() {
+    }
+}
